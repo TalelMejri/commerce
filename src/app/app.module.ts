eba4e95ce920b6c4ee8bbee5e519eacd500211f9
talel <talelmejri8@gmail.com>
2022-08-15 19:10:02 +0100

    import component in appmodule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 //import { ConsulteAllProductComponent } from "./core/component/consulte-all-product/consulte-all-product.component";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -13,10 +14,11 @@ import { CoreModule } from './core/core.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    CoreModule
+    CoreModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
-//tnajem direct t3ayet coremodule likbiir wala kol component wa7dou
+//tnajem direct t3ayet coremodule likbiir
