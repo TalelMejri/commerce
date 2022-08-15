55b5b8b8c42359395cba1efe05a81aad62276db0
talel <talelmejri8@gmail.com>
2022-08-15 19:10:55 +0100

    fixa add cproduit

--- src/app/core/component/add-produit/add-produit.component.ts
+++ src/app/core/component/add-produit/add-produit.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Product } from 'src/app/shared/model/Product';
+import { ProductService } from '../../services/api/product.service';
 
 @Component({
   selector: 'app-add-produit',
@@ -7,13 +10,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddProduitComponent implements OnInit {
 
-  constructor() { }
+  product:Product=new Product();
+  error:String="";
+  constructor(private productservice: ProductService,private router:Router ) { }
 
   ngOnInit(): void {
-    this.addproduct();
+    //this.addproduct();
   }
 
   addproduct(){
-
+    this.productservice.addproduct(this.product).subscribe(
+     data =>{this.product=data
+        this.router.navigate(['']);
+    },
+     err => {this.error=err.error.errorMessage},
+     () =>  {console.log('yay')})
   }
 }
