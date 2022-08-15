43f7fd42ca47e8f2b3d303c500acf903379e9436
talel <talelmejri8@gmail.com>
2022-08-15 19:09:08 +0100

    init angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddProduitComponent } from './core/component/add-produit/add-produit.component';
+import { ConsulteAllProductComponent } from './core/component/consulte-all-product/consulte-all-product.component';
+import { UpdateProductComponent } from './core/component/update-product/update-product.component';
+
 
 const routes: Routes = [
+  { path: '', component: ConsulteAllProductComponent },
+  { path: 'update/:id/:prix/:libelle', component: UpdateProductComponent },
+  { path: 'add', component: AddProduitComponent },
 
 ];
 
