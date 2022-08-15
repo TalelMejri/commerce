0f80f78c3ad831b9f86e44b0a61b828da229bfd3
talel <talelmejri8@gmail.com>
2022-08-15 19:12:23 +0100

    fix bugs in services

--- src/app/core/services/api/product.service.ts
+++ src/app/core/services/api/product.service.ts
@@ -16,8 +16,16 @@ export class ProductService {
     return this.http.get<Product[]>("http://localhost:8080/ConsulterProduct");
   }
 
-  addproduct():Observable<Product[]>{
-    return this.http.post<Product[]>("http://localhost:8080/AddProduitComponent",Product);
+  addproduct(product:Product):Observable<Product>{
+    return this.http.post<Product>("http://localhost:8080/addProduct",product);
+  }
+
+  updatedproduct(product:Product):Observable<Product> {
+    return this.http.put<Product>("http://localhost:8080/updateProduct",product);
+  }
+
+  deleteproduct(id:number){
+    return this.http.delete<Product>("http://localhost:8080/deleteProduit?id"+id);
   }
 
 }
